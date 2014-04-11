bef5ebf28bab7f17a43114ab807bb401b2f7923c
bhaity <bhaity@gmail.com>
2014-04-11 14:17:54 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MottFull::Application.config.secret_key_base = '266b9df742ff3459d623361e7467a72c1bbadd0eb06f5eeb1035a57bf20934baa9488963bed1ef6ab29f88000dded778fbc36e554456e005881359eeb819a1b0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MottFull::Application.config.session_store :cookie_store, key: '_mott-full_session'
