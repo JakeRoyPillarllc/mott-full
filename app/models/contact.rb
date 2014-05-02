edfda8914cd33d9814dc2625a85bd7b2cd59238b
Yogendra <yogendra@ignisitsolutions.com>
2014-05-02 19:56:02 +0530

    removed csv method from contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -32,11 +32,4 @@ class Contact < ActiveRecord::Base
   def send_contact_details_to_admin
     ContactMailer.send_contact_mail(self).deliver
   end
-
-  def self.to_csv
-    CSV.generate do |csv|
-      csv << column_names
-      csv << self.attributes.values_at(*column_names)
-    end
-  end
 end
