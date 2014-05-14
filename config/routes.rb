18c839f7d1f5a21b735ea2c138df41d3c9a792d0
Yogendra <yogendra@ignisitsolutions.com>
2014-05-14 20:39:23 +0530

    updated designing changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ MottFull::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'homes#index'
   resources :contacts
   resources :homes do
     collection do
